a540a871117ad0f3c48b6bd82638050917003509
caolu <chaolu@imudges.com>
2016-11-10 17:39:48 +0800

    homepage 1/4

--- app/src/main/java/com/example/rental/homepage/FindRentsFragment.java
+++ app/src/main/java/com/example/rental/homepage/FindRentsFragment.java
@@ -36,20 +36,23 @@ import java.util.List;
  */
 public class FindRentsFragment extends Fragment implements View.OnClickListener {
 
-    private TextView occupationTextView, hobbyTextView; //大分类按钮
-    private PopupWindow mPopupWindow1, mPopupWindow2;   //控件
-    private View popuWindow1, popuWindow2;              //布局
+    private TextView occupationTextView, hobbyTextView,numTextView; //大分类按钮
+    private PopupWindow mPopupWindow1, mPopupWindow2,mPopupWindow3;   //控件
+    private View popuWindow1, popuWindow2,popuWindow3;              //布局
     private int whereDisMiss = 0;                       //0;通过点击其他处隐藏，1:通过点击确定按钮隐藏
 
     private MyLinearLayouts[] mmLinearLayouts;          //大分类按键类数组
-    private int mmNum = 2;                              //大分类按键数量
+    private int mmNum = 3;                              //大分类按键数量
 
+    private final String[][] mT = new String[][]{{"程序员","公务员","教师","学生"},{"写代码","旅游"},{"2人","3人","4人","4人及以上"}};
 
     private PullToRefreshListView mListView;
     private RentsAdapter mAdapter;
     private static final String mUrl = "http://183.175.12.181:8000/hezuinfors";
     private int page = 1;          //上拉加载页面
     private List<RentInfoBean> mData;
+    private String[] exUrls ;
+    private boolean mFirst = true;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -61,37 +64,63 @@ public class FindRentsFragment extends Fragment implements View.OnClickListener 
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        initExUrl();
         initPopupWindow();
         initLayout();
         buttomTwoButtonClick();
         bounsView();
         initListView();
-        downloadInfo(true,0);
+        downloadInfo(mFirst, 0);
     }
 
+    private void initExUrl() {
+        exUrls = new String[mmNum];
+        for (int i = 0; i<mmNum;i++){
+            exUrls[i] = "";
+        }
+    }
+
+    /**
+     *
+     * @param first   第一次设置adapter 否则刷新数据
+     * @param mode    mode = 0;下拉刷新，mode = 1;上拉加载。
+     */
     private void downloadInfo(final boolean first, final int mode) {
         RequestParams param = new RequestParams();
-        param.put("page",page);
+        param.put("page", page);
+        param.put("Label1",exUrls[0]);
+        param.put("Label2",exUrls[1]);
+        param.put("Label3",exUrls[2]);
+        Log.i("exx",exUrls[0]+"*");
+        Log.i("exx",exUrls[1]+"*");
+        Log.i("exx",exUrls[2]+"*");
+
         try {
             RentInfoService.get(mUrl, param, new Listener() {
                 @Override
                 public void onSuccess(Object object) {
-                    RentInfoModel model = (RentInfoModel)object;
-                    mData = model.getHezudata();
-                    if (model == null){
-                        Toast.makeText(getContext(),"错误",Toast.LENGTH_SHORT).show();
-                    }else{
-                        if (model.getState() == 1){
+                    RentInfoModel model = (RentInfoModel) object;
+                    //mData = model.getHezudata();
+                    if (model == null) {
+                        Toast.makeText(getContext(), "错误", Toast.LENGTH_SHORT).show();
+                    } else {
+                        if (model.getState() == 1) {
                             if (mode == 1)
                                 mData.addAll(model.getHezudata());
-                            mAdapter.setData(mData);
-                            if (first)
-                                mListView.setAdapter(mAdapter);
                             else{
+                                mData = model.getHezudata();
+                            }
+                            mAdapter.setData(mData);
+                            if (first) {
+                                mListView.setAdapter(mAdapter);
+                                mFirst = false;
+                            }
+                            else {
                                 mAdapter.notifyDataSetChanged();
                             }
-                        }else{
-                            Toast.makeText(getContext(),model.getMsg(),Toast.LENGTH_SHORT).show();
+                            Log.i("refresh","yes");
+                        } else {
+                            Toast.makeText(getContext(), model.getMsg(), Toast.LENGTH_SHORT).show();
                         }
                     }
                     mListView.onRefreshComplete();
@@ -99,7 +128,7 @@ public class FindRentsFragment extends Fragment implements View.OnClickListener 
 
                 @Override
                 public void onFailure(String msg) {
-                    Toast.makeText(getContext(),"网络错误",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getContext(), "网络错误", Toast.LENGTH_SHORT).show();
                     mListView.onRefreshComplete();
                     if (mode == 1)
                         page--;
@@ -111,7 +140,7 @@ public class FindRentsFragment extends Fragment implements View.OnClickListener 
     }
 
     private void initListView() {
-        mListView = (PullToRefreshListView)getActivity().findViewById(R.id.homepagemain_fragment1_listview);
+        mListView = (PullToRefreshListView) getActivity().findViewById(R.id.homepagemain_fragment1_listview);
         mListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<ListView>() {
             @Override
             public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
@@ -120,7 +149,7 @@ public class FindRentsFragment extends Fragment implements View.OnClickListener 
                     mListView.onRefreshComplete();
                 }
                 page = 1;
-                downloadInfo(false,0);
+                downloadInfo(mFirst, 0);
             }
 
             @Override
@@ -130,7 +159,7 @@ public class FindRentsFragment extends Fragment implements View.OnClickListener 
                     mListView.onRefreshComplete();
                 }
                 page++;
-                downloadInfo(false,1);
+                downloadInfo(mFirst, 1);
             }
         });
         mAdapter = new RentsAdapter(getActivity());
@@ -142,12 +171,14 @@ public class FindRentsFragment extends Fragment implements View.OnClickListener 
     private void bounsView() {
         occupationTextView = (TextView) getActivity().findViewById(R.id.homepagemain_fragment_occupation);
         hobbyTextView = (TextView) getActivity().findViewById(R.id.homepagemain_fragment_hobby);
+        numTextView = (TextView) getActivity().findViewById(R.id.homepagemain_fragment_num);
+        numTextView.setOnClickListener(this);
         occupationTextView.setOnClickListener(this);
         hobbyTextView.setOnClickListener(this);
     }
 
     private void initLayout() {
-        mmLinearLayouts = new MyLinearLayouts[2];
+        mmLinearLayouts = new MyLinearLayouts[mmNum];
 
         mmLinearLayouts[0] = new MyLinearLayouts();
         mmLinearLayouts[0].setLayoutCount(4);
@@ -161,6 +192,13 @@ public class FindRentsFragment extends Fragment implements View.OnClickListener 
         mmLinearLayouts[1].setLayoutCount(2);
         mmLinearLayouts[1].mLinears[0] = (LinearLayout) popuWindow2.findViewById(R.id.homepagerents_popup2_check1);
         mmLinearLayouts[1].mLinears[1] = (LinearLayout) popuWindow2.findViewById(R.id.homepagerents_popup2_check2);
+
+        mmLinearLayouts[2] = new MyLinearLayouts();
+        mmLinearLayouts[2].setLayoutCount(4);
+        mmLinearLayouts[2].mLinears[0] = (LinearLayout) popuWindow3.findViewById(R.id.homepagerents_popup3_check1);
+        mmLinearLayouts[2].mLinears[1] = (LinearLayout) popuWindow3.findViewById(R.id.homepagerents_popup3_check2);
+        mmLinearLayouts[2].mLinears[2] = (LinearLayout) popuWindow3.findViewById(R.id.homepagerents_popup3_check3);
+        mmLinearLayouts[2].mLinears[3] = (LinearLayout) popuWindow3.findViewById(R.id.homepagerents_popup3_check4);
 
         for (int i = 0; i < mmNum; i++) {
             for (int j = 0; j < mmLinearLayouts[i].mLinears.length; j++) {
@@ -197,6 +235,21 @@ public class FindRentsFragment extends Fragment implements View.OnClickListener 
                     changeFlage(mmLinearLayouts[1].mLinears, 1);
             }
         });
+
+        popuWindow3 = getActivity().getLayoutInflater().inflate(R.layout.homepagerents_popup3, null);
+        mPopupWindow3 = new PopupWindow(popuWindow3, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+        mPopupWindow3.setTouchable(true);
+        mPopupWindow3.setOutsideTouchable(true);
+        mPopupWindow3.setBackgroundDrawable(getResources().getDrawable(R.drawable.ic_launcher));
+        mPopupWindow3.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                if (whereDisMiss == 0)
+                    changeFlage(mmLinearLayouts[2].mLinears, 1);
+            }
+        });
+
+
     }
 
     private void mmLinearLayoutsSetOnClick(final MyLinearLayouts mm) {
@@ -218,7 +271,7 @@ public class FindRentsFragment extends Fragment implements View.OnClickListener 
         TextView[] tvReset = new TextView[mmNum];
         tvReset[0] = (TextView) popuWindow1.findViewById(R.id.homepagerents_popup1_reset);
         tvReset[1] = (TextView) popuWindow2.findViewById(R.id.homepagerents_popup2_reset);
-
+        tvReset[2] = (TextView) popuWindow3.findViewById(R.id.homepagerents_popup3_reset);
         for (int k = 0; k < mmNum; k++) {
             final int j = k;
             tvReset[k].setOnClickListener(new View.OnClickListener() {
@@ -242,7 +295,8 @@ public class FindRentsFragment extends Fragment implements View.OnClickListener 
                 if (hasSelect(mmLinearLayouts[0].mLinears)) {
                     occupationTextView.setBackgroundResource(R.drawable.homepagerents_text_selectsome_selector);
                     occupationTextView.setTextColor(Color.RED);
-
+                    exUrls[0] = addUrl(mmLinearLayouts[0],0);
+                    downloadInfo(mFirst,0);
                 } else {
                     occupationTextView.setBackgroundResource(R.color.light_grey);
                     occupationTextView.setTextColor(Color.BLACK);
@@ -252,7 +306,6 @@ public class FindRentsFragment extends Fragment implements View.OnClickListener 
                 whereDisMiss = 0;
             }
         });
-
 
         TextView tv_2_ok = (TextView) popuWindow2.findViewById(R.id.homepagerents_popup2_ok);
         tv_2_ok.setOnClickListener(new View.OnClickListener() {
@@ -265,6 +318,8 @@ public class FindRentsFragment extends Fragment implements View.OnClickListener 
                     /**
                      * 增加网络请求，刷新listview
                      */
+                    exUrls[1] = addUrl(mmLinearLayouts[1],1);
+                    downloadInfo(mFirst,0);
                 } else {
                     hobbyTextView.setBackgroundResource(R.color.light_grey);
                     hobbyTextView.setTextColor(Color.BLACK);
@@ -274,8 +329,38 @@ public class FindRentsFragment extends Fragment implements View.OnClickListener 
                 whereDisMiss = 0;
             }
         });
+
+        TextView tv_3_ok = (TextView) popuWindow3.findViewById(R.id.homepagerents_popup3_ok);
+        tv_3_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                whereDisMiss = 1;
+                if (hasSelect(mmLinearLayouts[2].mLinears)) {
+                    numTextView.setBackgroundResource(R.drawable.homepagerents_text_selectsome_selector);
+                    numTextView.setTextColor(Color.RED);
+                    exUrls[2] = addUrl(mmLinearLayouts[2],2);
+                    downloadInfo(mFirst,0);
+                } else {
+                    numTextView.setBackgroundResource(R.color.light_grey);
+                    numTextView.setTextColor(Color.BLACK);
+                }
+                changeFlage(mmLinearLayouts[2].mLinears, 0);
+                mPopupWindow3.dismiss();
+                whereDisMiss = 0;
+            }
+        });
     }
 
+    private String addUrl(MyLinearLayouts mm,int which) {
+        String temp = "";
+        for (int i = 0; i < mm.mLinears.length; i++) {
+            MyFlag flag = (MyFlag) mm.mLinears[i].getTag();
+            if (flag.right == 1) {
+                temp = temp + mT[which][i] + "+";
+            }
+        }
+        return temp;
+    }
 
     private void changeViewColor(LinearLayout l, boolean setTag, int wred) {
         RelativeLayout r = (RelativeLayout) l.getChildAt(0);
@@ -356,6 +441,10 @@ public class FindRentsFragment extends Fragment implements View.OnClickListener 
             case R.id.homepagemain_fragment_hobby:
                 mPopupWindow2.showAsDropDown(view, 0, 10);
                 checkLinearLayout(mmLinearLayouts[1].mLinears);
+                break;
+            case R.id.homepagemain_fragment_num:
+                mPopupWindow3.showAsDropDown(view,0,10);
+                checkLinearLayout(mmLinearLayouts[2].mLinears);
                 break;
         }
 
